60924e5d0f3a4fc5e556be8244018a87e680eb34
ahmed <ahmed@gmail.com>
2021-10-20 17:40:09 +0200

    second-commit

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
